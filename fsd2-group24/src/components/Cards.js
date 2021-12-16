9982221bf76c28f6781c780cbd50085b25612884
anushavirri-r <anushavirri1004@gmail.com>
2021-12-16 19:48:26 +0530

    cards updated

--- fsd2-group24/src/components/Cards.js
+++ fsd2-group24/src/components/Cards.js
@@ -12,13 +12,11 @@ function Cards() {
             <CardItem
               src='images/Visakhapatnam.jpg'
               text='Visakhapatnam - the city of Destiny'
-              label='Adventure'
               path='/places'
             />
             <CardItem
               src='images/Hyderabad.jpg'
               text='Hyderabad - the city of Pearls'
-              label='Luxury'
               path='/services'
             />
           </ul>
@@ -26,19 +24,16 @@ function Cards() {
             <CardItem
               src='images/Tirupati.jpg'
               text='Tirumala & Tirupati - Spiritual City'
-              label='Mystery'
               path='/services'
             />
             <CardItem
               src='images/Araku.jpg'
               text='Araku - Ooty of Andhra'
-              label='Adventure'
               path='/products'
             />
             <CardItem
               src='images/Warangal.jpg'
               text='Warangal - Historical city of Telangana'
-              label='Adrenaline'
               path='/sign-up'
             />
           </ul>
