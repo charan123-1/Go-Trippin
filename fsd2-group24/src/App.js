95f645ec98d91d3b5ed4dbe98e2d78f8f456dd5d
anushavirri-r <anushavirri1004@gmail.com>
2021-12-16 16:13:18 +0530

    updated

--- fsd2-group24/src/App.js
+++ fsd2-group24/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer';
 import Shop from './pages/Shop';
 import Item from './pages/Item';
-import Feedback from './pages/Feedback';
+// import Feedback from './pages/Feedback';
 import Travel from './pages/Travel';
 import Adventure from './pages/Adventure';
 import  MapContainer  from './components/maps';
@@ -21,7 +21,7 @@ function App() {
           <Route path="/todo" element={<TodoList />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/item" element={<Item />} />
-          <Route path="/feedback" element={<Feedback />} />
+          {/* <Route path="/feedback" element={<Feedback />} /> */}
           <Route path="/travelessential" element={<Travel />} />
           <Route path="/adventure" element={<Adventure />} />
           <Route path="/places" element={<MapContainer />} />
--- fsd2-group24/src/components/maps.js
+++ fsd2-group24/src/components/maps.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Map, Marker, GoogleApiWrapper} from 'google-maps-react';
-=======
-import { Map,Marker, GoogleApiWrapper, } from 'google-maps-react';
->>>>>>> 9a2bd4420dafb3a36711e7f846452261b5432241
 
 const mapStyles = {
   width: '100%',
@@ -17,7 +13,6 @@ export class MapContainer extends Component {
         google={this.props.google}
         zoom={10}
         style={mapStyles}
-<<<<<<< HEAD
         initialCenter={{
             lng: 83.323685,
             lat: 17.714294
@@ -37,32 +32,6 @@ export class MapContainer extends Component {
             color: "black"
           }}
         />
-=======
-        initialCenter={
-          {
-            lng: 83.323685,
-                        lat:  17.714294
-          }
-        }>
-        <Marker key="marker_1"
-
-                    position={{
-
-                        lng: 83.323685,
-                        lat:  17.714294
-
-                    }}
-                    label={{
-                            text: "RK BEACH",
-                            fontFamily: "Arial",
-                            fontSize: "20px",
-                            color:"black"
-                    }}
-
-                />
-
-
->>>>>>> 9a2bd4420dafb3a36711e7f846452261b5432241
       </Map>
     );
   }
