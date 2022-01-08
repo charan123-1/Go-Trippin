2c7ea1afde402a4826984c65bac1b1668342cb45
anushavirri-r <anushavirri1004@gmail.com>
2022-01-08 14:04:02 +0530

    map updated

--- fsd2-group24/src/components/maps.js
+++ fsd2-group24/src/components/maps.js
@@ -13,6 +13,7 @@ const map_img ={
 
 }
 
+
 export class MapContainer extends Component {
     state = {
     showingInfoWindow: false,
@@ -40,31 +41,124 @@ export class MapContainer extends Component {
     return (
       <Map google={this.props.google}
           onClick={this.onMapClicked}
-          initialCenter={{
-            lng: 78.47470741698163,
-            lat: 17.361696696371606
-
-          }}
+        style={mapStyles}
+        initialCenter={{
+          lng: 83.323685,
+          lat: 17.714294
+        }}
           >
 
         <Marker onMouseover={this.onMarkerClick}
         position={{
 
-            lng: 78.47470741698163,
-            lat: 17.361696696371606
+          lng: 83.323685,
+          lat: 17.714294
 
           }}
-                name={'Hyderabad'}
+                name={'RK beach'}
                 />
 
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}>
             <div style={map_img} >
-              <img src="images/ABYS.jpg" width="300px" height="300px;"/>
+              <img src="images/RKbeach.jpg" width="300px" height="300px;"/>
               <h3>{this.state.selectedPlace.name}</h3>
             </div>
         </InfoWindow>
+
+        <Marker key="marker_1"
+          onMouseover={this.onMarkerClick}
+          position={{
+
+            lng: 83.34519670139929,
+            lat: 17.748298645238204
+
+          }}
+          name={'KAILASH GIRI'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div style={map_img} >
+            <img src="images/RKbeach.jpg" width="300px" height="300px;" />
+            <h3>{this.state.selectedPlace.name}</h3>
+          </div>
+        </InfoWindow>
+
+        <Marker key="marker_1"
+          onMouseover={this.onMarkerClick}
+          position={{
+
+            lng: 83.34854542564264,
+            lat: 17.74734806039722
+
+          }}
+          name={"TENNETI PARK"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div style={map_img} >
+            <img src="images/RKbeach.jpg" width="300px" height="300px;" />
+            <h3>{this.state.selectedPlace.name}</h3>
+          </div>
+        </InfoWindow>
+        <Marker key="marker_1"
+          onMouseover={this.onMarkerClick}
+          position={{
+
+            lng: 83.33003082944325,
+            lat: 17.717481135950656
+
+          }}
+          name={"Kursura Submarine"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div style={map_img} >
+            <img src="images/RKbeach.jpg" width="300px" height="300px;" />
+            <h3>{this.state.selectedPlace.name}</h3>
+          </div>
+        </InfoWindow>
+        <Marker key="marker_1"
+          onMouseover={this.onMarkerClick}
+          position={{
+
+            lng: 83.34425427531079,
+            lat: 17.768985164595804
+
+          }}
+          name={"Indira Gandhi Zoological Park"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div style={map_img} >
+            <img src="images/RKbeach.jpg" width="300px" height="300px;" />
+            <h3>{this.state.selectedPlace.name}</h3>
+          </div>
+        </InfoWindow>
+        <Marker key="marker_1"
+          onMouseover={this.onMarkerClick}
+          position={{
+
+            lng: 83.29508559851892,
+            lat: 17.698508099513553
+
+          }}
+          name={"Sri Kanaka Maha Lakshmi Temple"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div style={map_img} >
+            <img src="images/RKbeach.jpg" width="300px" height="300px;" />
+            <h3>{this.state.selectedPlace.name}</h3>
+          </div>
+        </InfoWindow>
+
       </Map>
     );
   }
